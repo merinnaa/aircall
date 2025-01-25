16f3b9df6c2451344332fc539d46fb7b661d80ae
luwam <lwamyohannes2018@gmail.com>
2025-01-24 19:29:46 -0700

    Debug error

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,33 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
   module: {
-   
-    
     rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
-      },
-      
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
     })
-  ]
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
-
